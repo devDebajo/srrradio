55b70dd36752f7f1035495da4b8aa4ed7c78e9f9
debajo <nikitosbobin@gmail.com>
2022-05-22 23:35:28 +0500

    fix debug

--- common-di/src/main/java/ru/debajo/srrradio/common/di/KoinModuleContext.kt
+++ common-di/src/main/java/ru/debajo/srrradio/common/di/KoinModuleContext.kt
@@ -2,6 +2,7 @@ package ru.debajo.srrradio.common.di
 
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.KoinContext
 import org.koin.core.error.KoinAppAlreadyStartedException
 import org.koin.core.module.Module
@@ -10,54 +11,74 @@ import org.koin.dsl.KoinAppDeclaration
 internal class KoinModuleContext : KoinContext {
 
     private var _koin: Koin? = null
-    private var _koinApplication: KoinApplication? = null
 
     override fun get(): Koin = _koin ?: error("KoinApplication has not been started")
 
     override fun getOrNull(): Koin? = _koin
 
-    fun getKoinApplicationOrNull(): KoinApplication? = _koinApplication
+    override fun register(koinApplication: KoinApplication) {
+        if (_koin != null) {
+            throw KoinAppAlreadyStartedException("A Koin Application has already been started")
+        }
+        _koin = koinApplication.koin
+    }
 
-    override fun stopKoin() = synchronized(this) {
+    override fun stop() = synchronized(this) {
         _koin?.close()
         _koin = null
     }
 
-    override fun startKoin(koinApplication: KoinApplication): KoinApplication = synchronized(this) {
-        register(koinApplication)
+    /**
+     * Start a Koin Application as StandAlone
+     */
+    internal fun startKoin(
+        koinContext: KoinContext = GlobalContext,
+        koinApplication: KoinApplication
+    ): KoinApplication = synchronized(this) {
+        koinContext.register(koinApplication)
         koinApplication.createEagerInstances()
         return koinApplication
     }
 
-    override fun startKoin(appDeclaration: KoinAppDeclaration): KoinApplication = synchronized(this) {
+    /**
+     * Start a Koin Application as StandAlone
+     */
+    internal fun startKoin(
+        koinContext: KoinContext = GlobalContext,
+        appDeclaration: KoinAppDeclaration
+    ): KoinApplication = synchronized(this) {
         val koinApplication = KoinApplication.init()
-        register(koinApplication)
+        koinContext.register(koinApplication)
         appDeclaration(koinApplication)
         koinApplication.createEagerInstances()
         return koinApplication
     }
 
-    override fun loadKoinModules(module: Module) = synchronized(this) {
+    /**
+     * load Koin module in global Koin context
+     */
+    internal fun loadKoinModules(module: Module) = synchronized(this) {
         get().loadModules(listOf(module))
     }
 
-    override fun loadKoinModules(modules: List<Module>) = synchronized(this) {
+    /**
+     * load Koin modules in global Koin context
+     */
+    internal fun loadKoinModules(modules: List<Module>) = synchronized(this) {
         get().loadModules(modules)
     }
 
-    override fun unloadKoinModules(module: Module) = synchronized(this) {
+    /**
+     * unload Koin modules from global Koin context
+     */
+    internal fun unloadKoinModules(module: Module) = synchronized(this) {
         get().unloadModules(listOf(module))
     }
 
-    override fun unloadKoinModules(modules: List<Module>) = synchronized(this) {
+    /**
+     * unload Koin modules from global Koin context
+     */
+    internal fun unloadKoinModules(modules: List<Module>) = synchronized(this) {
         get().unloadModules(modules)
-    }
-
-    private fun register(koinApplication: KoinApplication) {
-        if (_koin != null) {
-            throw KoinAppAlreadyStartedException("A Koin Application has already been started")
-        }
-        _koinApplication = koinApplication
-        _koin = koinApplication.koin
     }
 }
