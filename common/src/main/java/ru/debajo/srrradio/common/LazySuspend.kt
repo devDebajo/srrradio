c5608ef3ef5b46d1a1656d0db8bae2892ef62c5d
debajo <nikitosbobin@gmail.com>
2022-07-21 00:22:23 +0500

    added lazySuspend

---
+++ common/src/main/java/ru/debajo/srrradio/common/LazySuspend.kt
@@ -0,0 +1,42 @@
+package ru.debajo.srrradio.common
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+interface LazySuspend<T> {
+    suspend fun get(): T
+}
+
+interface MutableLazySuspend<T> : LazySuspend<T> {
+    suspend fun set(value: T)
+}
+
+private class LazySuspendImpl<T>(
+    private val factory: suspend () -> T,
+) : MutableLazySuspend<T> {
+
+    @Volatile
+    private var value: T? = null
+    private val mutex = Mutex()
+
+    override suspend fun get(): T {
+        if (value == null) {
+            mutex.withLock {
+                if (value == null) {
+                    value = factory()
+                }
+            }
+        }
+        return value!!
+    }
+
+    override suspend fun set(value: T) {
+        mutex.withLock {
+            this@LazySuspendImpl.value = value
+        }
+    }
+}
+
+fun <T> lazySuspend(factory: suspend () -> T): LazySuspend<T> = mutableLazySuspend(factory)
+
+fun <T> mutableLazySuspend(factory: suspend () -> T): MutableLazySuspend<T> = LazySuspendImpl(factory)
