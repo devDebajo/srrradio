aa6d7a4a9d610f7d7a40557dd3b773b61c17d271
debajo <nikitosbobin@gmail.com>
2022-08-28 00:06:35 +0500

    implement merger

--- app/src/main/java/ru/debajo/srrradio/sync/AppStateSnapshotMerger.kt
+++ app/src/main/java/ru/debajo/srrradio/sync/AppStateSnapshotMerger.kt
@@ -1,9 +1,21 @@
 package ru.debajo.srrradio.sync
 
 import ru.debajo.srrradio.domain.model.AppStateSnapshot
+import ru.debajo.srrradio.domain.model.Timestamped
 
 internal class AppStateSnapshotMerger {
     fun merge(local: AppStateSnapshot, forSync: AppStateSnapshot): AppStateSnapshot {
-        TODO()
+        return AppStateSnapshot(
+            dynamicIcon = local.dynamicIcon mergeWith forSync.dynamicIcon,
+            themeCode = local.themeCode mergeWith forSync.themeCode,
+            autoSendErrors = local.autoSendErrors mergeWith forSync.autoSendErrors,
+
+            collection = (local.collection + forSync.collection).distinct(),
+            favoriteStations = (local.favoriteStations + forSync.favoriteStations).distinctBy { it.id },
+        )
+    }
+
+    private infix fun <T> Timestamped<T>.mergeWith(other: Timestamped<T>): Timestamped<T> {
+        return if (timestamp > other.timestamp) this else other
     }
 }
